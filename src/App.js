ac87b4eac6c381219b695b0d3a2c81869601cc6e
fanni <anwarainul>
2020-08-31 23:14:21 +0700

    pk_day_11

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo './logo.svg';
+import "./App.test";
+import Navbar from "./Component/Pages/navbar";
+import Content from ".Component/Pages/Content";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+        <navbar />
+        <Content />
+        </div>
+    );
 }
-
 export default App;
---
+++ src/component/pages/content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../../component/button/Button'
+
+const content = () => {
+    return (
+        <div>
+            <h1>Hollo Word</h1>
+            <Button text='Test Button' />
+        </div>
+    )
+}
+export default content
